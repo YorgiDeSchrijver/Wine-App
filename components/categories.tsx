a4b10114ec2d1705e04e62435e3d7bad18fcd3fb
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2024-12-04 17:30:57 +0100

    configured colors

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,6 +1,6 @@
 import { Component, useState } from "react";
 import { FlatList, Text, TouchableOpacity, View } from "react-native";
-import categories from "../app/config/categories";
+import categories from "../config/categories";
 
 interface CategoriesProps {
   onchange: (id: number) => void;
@@ -19,19 +19,19 @@ export default function Categories({ onchange }: CategoriesProps) {
       horizontal={true}
       data={categories}
       keyExtractor={(item) => item.id.toString()}
-      className="mx-3"
+      className="my-3"
       renderItem={({ item }) => (
         <TouchableOpacity
           onPress={() => handlePress(item.id)}
-          className="mr-3 items-center"
+          className="mr-5 items-center"
         >
           <Text
-            className={`text-xl text-secondary-950 ${activeCategoryId === item.id && "text-primary-950"}`}
+            className={`text-[20px]  ${activeCategoryId === item.id ? "text-primary": "text-secondary"}`}
           >
             {item.name}
           </Text>
           {activeCategoryId === item.id && (
-            <View className="mt-1 h-3 w-3 rounded-md bg-primary-950" />
+            <View className="mt-1 h-3 w-3 rounded-md bg-primary" />
           )}
         </TouchableOpacity>
       )}
